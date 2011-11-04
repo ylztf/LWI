4e4c96060a4343fa4490062a13ce409667ea6694
Yaxi Liu <ylztf@mst.edu>
2011-11-04 18:18:01 -0500

    more

---
+++ Broker/src/CDieselGeneratorDevice.cpp
@@ -0,0 +1,82 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file CDieselGeneratorDevice.cpp
+///
+/// @author Yaxi Liu <ylztf@mst.edu>
+///
+/// @compiler C++
+///
+/// @project FREEDM DGI
+///
+/// @description Manages the solar panels
+///
+/// @license
+/// These source code files were created at as part of the
+/// FREEDM DGI Subthrust, and are
+/// intended for use in teaching or research. They may be
+/// freely copied, modified and redistributed as long
+/// as modified versions are clearly marked as such and
+/// this notice is not removed.
+///
+/// Neither the authors nor the FREEDM Project nor the
+/// National Science Foundation
+/// make any warranty, express or implied, nor assumes
+/// any legal responsibility for the accuracy,
+/// completeness or usefulness of these codes or any
+/// information distributed with these codes.
+///
+/// Suggested modifications or questions about these codes
+/// can be directed to Dr. Bruce McMillin, Department of
+/// Computer Science, Missour University of Science and
+/// Technology, Rolla, MO 65409 (ff@mst.edu).
+///////////////////////////////////////////////////////////////////////////////
+
+#include "CDieselGeneratorDevice.hpp"
+
+namespace freedm {
+	namespace broker {
+
+		///////////////////////////////////////////////////////////////////////////////
+		/// @fn CDIESELGENERATORDEVICE
+		/// @brief  construcctor for the diesel generator.
+        ///         Its type is always DG.
+		/// @param phymanager The related physical device manager.
+		/// @param deviceid The identifier for this generic device.
+		/// @param lineClient  the client that connects to the PSCAD interface
+		///////////////////////////////////////////////////////////////////////////////
+		CDieselGeneratorDevice::CDieselGeneratorDevice(CLineClient::TPointer lineClient, CPhysicalDeviceManager& phymanager, IPhysicalDevice::Identifier deviceid)  
+			: CPSCADDevice(lineClient, phymanager, deviceid, physicaldevices::DG)
+		{};
+
+		/////////////////////////////////////////////////////////////////////////////
+		/// @fn GET_POWERLEVEL
+		/// @brief get the generated power level of the diesel generator
+		/// @return  the generated power level read from PSCAD simulation
+		///////////////////////////////////////////////////////////////////////////////
+		CDieselGeneratorDevice:: SettingValue CDieselGeneratorDevice::get_powerLevel()
+		{
+			CPSCADDevice::Get("powerLevel");
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		/// @fn TURNON
+		/// @brief turn diesel generator on
+		///////////////////////////////////////////////////////////////////////////////
+		void CDieselGeneratorDevice::turnOn()
+		{
+			CPSCADDevice::Set("onOffSwitch", 1);
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		/// @fn TURNOFF
+		/// @brief turn diesel generator off
+		///////////////////////////////////////////////////////////////////////////////
+		void CDieselGeneratorDevice::turnOff()
+		{
+			CPSCADDevice::Set("onOffSwitch", 0);
+		}
+
+	} // namespace broker
+} // namespace freedm
+
+
+
---
+++ Broker/src/CGridLinkDevice.cpp
@@ -0,0 +1,82 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file CGridLinkDevice.cpp
+///
+/// @author Yaxi Liu <ylztf@mst.edu>
+///
+/// @compiler C++
+///
+/// @project FREEDM DGI
+///
+/// @description Manages the solar panels
+///
+/// @license
+/// These source code files were created at as part of the
+/// FREEDM DGI Subthrust, and are
+/// intended for use in teaching or research. They may be
+/// freely copied, modified and redistributed as long
+/// as modified versions are clearly marked as such and
+/// this notice is not removed.
+///
+/// Neither the authors nor the FREEDM Project nor the
+/// National Science Foundation
+/// make any warranty, express or implied, nor assumes
+/// any legal responsibility for the accuracy,
+/// completeness or usefulness of these codes or any
+/// information distributed with these codes.
+///
+/// Suggested modifications or questions about these codes
+/// can be directed to Dr. Bruce McMillin, Department of
+/// Computer Science, Missour University of Science and
+/// Technology, Rolla, MO 65409 (ff@mst.edu).
+///////////////////////////////////////////////////////////////////////////////
+
+#include "CGridLinkDevice.hpp"
+
+namespace freedm {
+	namespace broker {
+
+		///////////////////////////////////////////////////////////////////////////////
+		/// @fn CGridLinkDevice
+		/// @brief  construcctor for the AC power line connecting a PMCU to the main grid.
+        ///         Its type is always GRID.
+		/// @param phymanager The related physical device manager.
+		/// @param deviceid The identifier for this generic device.
+		/// @param lineClient  the client that connects to the PSCAD interface
+		///////////////////////////////////////////////////////////////////////////////
+		CGridLinkDevice::CGridLinkDevice(CLineClient::TPointer lineClient, CPhysicalDeviceManager& phymanager, IPhysicalDevice::Identifier deviceid)  
+			: CPSCADDevice(lineClient, phymanager, deviceid, physicaldevices::GRID)
+		{};
+
+		/////////////////////////////////////////////////////////////////////////////
+		/// @fn GET_POWERLEVEL
+		/// @brief get the generated power level of the power line 
+		/// @return  the generated power level read from PSCAD simulation
+		///////////////////////////////////////////////////////////////////////////////
+		CGridLinkDevice:: SettingValue CGridLinkDevice::get_powerLevel()
+		{
+			CPSCADDevice::Get("powerLevel");
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		/// @fn turnOn
+		/// @brief turn the breaker on the powerline off, so power flows
+		///////////////////////////////////////////////////////////////////////////////
+		void CGridLinkDevice::turnOn()
+		{
+			CPSCADDevice::Set("onOffSwitch", 1);
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		/// @fn turnOff
+		/// @brief turn the breaker on the power line on, so power stops flowing
+		///////////////////////////////////////////////////////////////////////////////
+		void CGridLinkDevice::turnOff()
+		{
+			CPSCADDevice::Set("onOffSwitch", 0);
+		}
+
+	} // namespace broker
+} // namespace freedm
+
+
+
